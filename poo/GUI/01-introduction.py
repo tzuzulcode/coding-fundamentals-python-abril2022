1c2bb06626735ebd8fe797c3f05d92f40bf5b2a6
Tzuzul <mail@tzuzulcode.com>
2022-06-03 17:04:39 -0500

    GUI

--- poo/GUI/01-introduction.py
+++ poo/GUI/01-introduction.py
@@ -4,7 +4,7 @@ import tkinter as tk
 # master widget
 window = tk.Tk() # widget que referencía a la ventana raiz
 window.title("Primer programa")
-window.geometry("300x300+300+300") # WIDTHxHEIGHT+POSITIONX+POSITIONY
+window.geometry("500x300+300+300") # WIDTHxHEIGHT+POSITIONX+POSITIONY
 window.resizable(True,False) # WIDTH,HEIGHT
 
 # label -> etiqueta
@@ -24,12 +24,14 @@ cats_input = tk.Checkbutton(window,text="Click si tienes gatos!!") # checkbox
 number_label = tk.Label(window,text="Cuantos gatos tienes?")
 number_of_cats = tk.Spinbox(window,from_=0,to=100,increment=1)
 
-label.grid(columnspan=2) # Colocamos el elemento en la interfaz
-name_label.grid(row=1,column=0)
+# pady=(top,bottom), padx=(left,right)
+# pady=(arriba,abajo), padx=(izquierda,derecha)
+label.grid(columnspan=2, pady=(0,10), ipady=20, ipadx=20,sticky=(tk.W+tk.E)) # Colocamos el elemento en la interfaz
+name_label.grid(row=1,column=0, padx=50,pady=20)
 name_input.grid(row=1,column=1)
 cats_input.grid(row=2,column=1)
 number_label.grid(row=3,column=0)
-number_of_cats.grid(row=3,column=1)
+number_of_cats.grid(row=3,column=1, padx=50, pady=20)
 
 
 window.mainloop() # Inicia el ciclo de eventos de la app.
